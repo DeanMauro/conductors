7ba923128cffd38f7535a8f7ce9eb67d3abafdea
Dean Mauro <DeanMauro@users.noreply.github.com>
2017-12-21 10:32:18 -0500

    Add files via upload

---
+++ ruby/test.rb
@@ -0,0 +1,26 @@
+require_relative 'orchestrator'
+
+#________________________________________________________________#
+#                    CREATE ORCHESTRATOR OBJECT                  #
+#                  (Automatically authenticates)                 #
+#     Params: tenant, username, password, [orchestrator url]     #
+#________________________________________________________________#
+
+orch = Orchestrator.new("tenant", "username", "password")
+
+#________________________________________________________________#
+#                          SEND REQUEST                          #
+#          Params: request type, url extension, [data]           #
+#________________________________________________________________#
+
+# GET
+res = orch.request('get', 'odata/Environments')
+puts res["body"]["value"]
+
+# POST
+res = orch.request('post', 'odata/Assets', {Name: "Asset" + Random.rand(99999).to_s,
+											ValueScope: "Global",
+											ValueType: "Text",
+											Value: "Et tu asset 2",
+  											StringValue: "Et tu asset 2"})
+puts res["body"]
