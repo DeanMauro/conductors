a63e3e504a80b1914ea0e2d332b85170a42be440
Dean Mauro <DeanMauro@users.noreply.github.com>
2017-12-28 12:25:28 -0500

    Add files via upload

---
+++ python/orchestrator.py
@@ -0,0 +1,23 @@
+import requests
+
+class Orchestrator:
+	token = None
+
+	def __init__(self, tenant, user, password, url = 'https://platform.uipath.com/'):
+		self.url = url
+		self.token = self.__getToken(tenant, user, password)
+
+
+	def __getToken(cls, tenant, user, password):
+		res = cls.request('POST', 'api/account/authenticate', {'tenancyName': tenant,
+						                                   	   'usernameOrEmailAddress': user, 
+						                                   	   'password': password})
+		return res["result"]
+
+
+	def request(self, type, extension, body = None):
+		uri = self.url + extension
+		headers = {'Authorization': 'Bearer ' + (self.token or '')}
+
+		response = requests.request(type.upper(), uri, data=body, headers=headers)
+		return response.json()
---
+++ python/test.py
@@ -0,0 +1,27 @@
+from orchestrator import Orchestrator
+import uuid
+
+#________________________________________________________________#
+#                    CREATE ORCHESTRATOR OBJECT                  #
+#                  (Automatically authenticates)                 #
+#     Params: tenant, username, password, [orchestrator url]     #
+#________________________________________________________________#
+
+orch = Orchestrator("tenant", "username", "password")
+
+#________________________________________________________________#
+#                          SEND REQUEST                          #
+#          Params: request type, url extension, [data]           #
+#________________________________________________________________#
+
+"""GET"""
+res = orch.request('get', 'odata/Environments')
+print(res)
+
+"""POST"""
+res = orch.request('post', 'odata/Assets', {'Name': "Asset " + str(uuid.uuid4())[0:8],
+											'ValueScope': "Global",
+											'ValueType': "Text",
+											'Value': "Et tu asset 2",
+  											'StringValue': "Et tu asset 2"})
+print(res)
